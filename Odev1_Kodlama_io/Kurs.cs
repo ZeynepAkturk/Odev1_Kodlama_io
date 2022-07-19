30e0c421c64bd159fc2e339f4c469496afcee42a
Zeynep Akturk <zeynepakturk.maibordo@gmail.com>
2022-07-19 14:05:05 +0300

    Proje dosyası ekle.

---
+++ Odev1_Kodlama_io/Kurs.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Odev1_Kodlama_io
+{
+    
---
+++ Odev1_Kodlama_io/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Odev1_Kodlama_io
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kurs JavaScKurs = new Kurs();
+            JavaScKurs.KursAdi = " JavaSCript Yazılımcı Kampı";
+            JavaScKurs.KursEgitmen = "Engin DEMİROĞ";
+            Kurs CAngularKurs = new Kurs();
+            CAngularKurs.KursAdi = " C# Yazılım Geliştirici Kampı";
+            CAngularKurs.KursEgitmen = "Engin Demiroğ";
+            Kurs JavaKurs= new Kurs();
+            JavaKurs.KursAdi = " Java & React Yazılım Geliştirici Kampı";
+            JavaKurs.KursEgitmen = "Engin Demiroğ";
+
+            Kurs[] kurslarim = new Kurs[] {JavaScKurs, CAngularKurs,JavaKurs};
+
+
+            foreach (Kurs kursum in kurslarim)
+            {
+                Console.WriteLine(kursum.KursAdi);
+                Console.WriteLine(kursum.KursEgitmen);
+
+            }
+
+
+
+
+
+        }
+    }
+    class Kurs
+    {
+        public string KursAdi { get; set; }
+        public string KursEgitmen { get; set; }
+
+    }
+}
+
